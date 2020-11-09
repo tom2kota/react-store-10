2b8c83e83a66d7e7c2e80e6d01b10b63b71803f6
Web Dev <tom2kota@gmail.com>
2020-11-09 22:29:09 +0200

    Replace Redux with Context API on Checkout page for Cart

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -1,48 +1,52 @@
-import React from "react";
+import React, {useContext} from "react";
 import {createStructuredSelector} from "reselect";
-import {selectCartItems, selectCartTotal} from "../../redux/cart/cartSelectors";
+import {selectCartTotal} from "../../redux/cart/cartSelectors";
 import {connect} from "react-redux";
 import CheckoutItem from "../../components/checkout-item/CheckoutItem";
 import {StripeCheckoutButton} from "../../components/stripe-button/StripeButton";
+import {CartContext} from "../../providers/cart/cartProvider";
 import './CheckoutPage.scss'
 
-const CheckoutPage = ({cartItems, total}) => (
-    <div className='checkout-page'>
+const CheckoutPage = ({total}) => {
+    const {cartItems} = useContext(CartContext)
 
-        <div className='checkout-header'>
-            <div className='header-block'>
-                <span>Product</span>
-            </div>
-            <div className='header-block'>
-                <span>Description</span>
-            </div>
-            <div className='header-block'>
-                <span>Quantity</span>
-            </div>
-            <div className='header-block'>
-                <span>Price</span>
-            </div>
-            <div className='header-block'>
-                <span>Remove</span>
-            </div>
-        </div>
+    return (
+        <div className='checkout-page'>
 
-        {cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem}/>)}
+            <div className='checkout-header'>
+                <div className='header-block'>
+                    <span>Product</span>
+                </div>
+                <div className='header-block'>
+                    <span>Description</span>
+                </div>
+                <div className='header-block'>
+                    <span>Quantity</span>
+                </div>
+                <div className='header-block'>
+                    <span>Price</span>
+                </div>
+                <div className='header-block'>
+                    <span>Remove</span>
+                </div>
+            </div>
 
-        <div className='total'>
-            <span>Total: ${total}</span>
+            {cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem}/>)}
+
+            <div className='total'>
+                <span>Total: ${total}</span>
+            </div>
+            <div className='warning'>
+                <h2>&#9888;  Use the following credit cards for payments !</h2>
+                <p>4242 4242 4242 4242 (Visa), CVC - Any 3 digits, Date - Any future date </p>
+                <p>5555 5555 5555 4444 (Mastercard), CVC - Any 3 digits, Date - Any future date</p>
+            </div>
+            <StripeCheckoutButton price={total}/>
         </div>
-        <div className='warning'>
-            <h2>&#9888;  Use the following credit cards for payments !</h2>
-            <p>4242 4242 4242 4242 (Visa), CVC - Any 3 digits, Date - Any future date </p>
-            <p>5555 5555 5555 4444 (Mastercard), CVC - Any 3 digits, Date - Any future date</p>
-        </div>
-        <StripeCheckoutButton price={total}/>
-    </div>
-)
+    )
+}
 
 const mapStateToProps = createStructuredSelector({
-    cartItems: selectCartItems,
     total: selectCartTotal
 })
 
