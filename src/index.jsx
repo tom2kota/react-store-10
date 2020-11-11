e34b7a87f8e0f25ebf9e527650be9ea4799d45ab
Web Dev <tom2kota@gmail.com>
2020-11-12 01:32:42 +0200

    Remove all Redux files

--- src/index.jsx
+++ src/index.jsx
@@ -2,27 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {App} from './components/app/App';
-import * as serviceWorker from './serviceWorker';
-import {GlobalStyle} from "./globalStyles";
-import {Provider} from 'react-redux';
-import {store, reduxPersist} from "./redux/store";
-import {PersistGate} from "redux-persist/integration/react";
 import {CartProvider} from "./providers/cart/cartProvider";
+import {GlobalStyle} from "./globalStyles";
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <React.StrictMode>
         <CartProvider>
-            <Provider store={store}>
-                <BrowserRouter>
-                    <GlobalStyle/>
-                    <PersistGate persistor={reduxPersist}>
-                        <App/>
-                    </PersistGate>
-                </BrowserRouter>
-            </Provider>
+            <BrowserRouter>
+                <GlobalStyle/>
+                <App/>
+            </BrowserRouter>
         </CartProvider>
-    </React.StrictMode>
-    , document.getElementById('root')
-);
+    </React.StrictMode>,
+    document.getElementById('root')
+)
 
-serviceWorker.unregister();
+serviceWorker.unregister()
